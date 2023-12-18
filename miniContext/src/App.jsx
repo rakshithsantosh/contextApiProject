47e5851ec2cb1f2a09433baba030267169e18a71
rakshith_s <rakshithsantosh@gmail.com>
2023-12-18 08:25:10 +0530

    context api

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Login from './components/login'
+import Profile from './components/profile'
+import UserContextProvider from './context/UserContextProvider'
+
+
+function App() {
+ 
+  return (
+    <UserContextProvider >
+    <h1>context Api</h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
